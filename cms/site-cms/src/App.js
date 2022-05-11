38ede974ff9532cc81f36990431c9d6c45ecce8f
mihco <69523822+mihco@users.noreply.github.com>
2022-05-11 18:56:39 -0400

    beginning work on simple blogging CMS with react

---
+++ cms/site-cms/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="header">
+
+      </div>
+      <div classname="content">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ cms/site-cms/src/Post.js
@@ -0,0 +1,11 @@
+
+function Post() {
+    return (
+      <div>
+          <div classname="post-title"></div>
+          <div></div>
+      </div>
+    );
+  }
+  
+  export default App;
