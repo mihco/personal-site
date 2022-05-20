fda8dbd3deb08ff88f57be6213135af07f815eda
mihco <69523822+mihco@users.noreply.github.com>
2022-05-19 23:15:25 -0400

    Added top navbar for blog, and dividers between blog posts

--- cms/site-cms/src/App.js
+++ cms/site-cms/src/App.js
@@ -15,12 +15,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="header">
-          <h1 className='header-content'>Blog</h1>
-          <h1 className='header-content'><a href="">Home</a></h1>
+        <div className="header bg-amber-200 fixed opacity-80 block">
+          <ul>
+            <li className='header-content p-4'>Blog</li>
+            <li className='header-content p-4 absolute right-0'><a href="">Home</a></li>
+          </ul>
         </div>
-        <div className="content">
-          { this.state.loadedContent ? this.state.loadedContent.map((item) => <Post key={item} image="" title="Hello" date={item} content={loremIpsum["loremIpsum"]}/>) : <h1>Loading</h1>}
+        <div className="content block">
+          { this.state.loadedContent ? this.state.loadedContent.map((item) => <div>{ item === 1 ? "" : <hr className='border-t-amber-300 border-t-4 border-dashed'></hr> }<Post key={item} image="" title="Hello" date={item} content={loremIpsum["loremIpsum"]}/></div>) : <h1>Loading</h1>}
         </div>
       </div>
     )
