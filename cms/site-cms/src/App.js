3e220f8a02d3bcbd65692aab81883354809c1e98
mihco <69523822+mihco@users.noreply.github.com>
2022-06-28 23:55:54 -0400

    Attempted to make fetch function work, failed :(

--- cms/site-cms/src/App.js
+++ cms/site-cms/src/App.js
@@ -4,6 +4,8 @@ import Post from './Post'
 import loremIpsum from './LoremIpsum';
 import './App.css';
 
+const concat = require("concat-stream")
+
 class App extends React.Component {
   constructor() {
     super();
@@ -13,20 +15,20 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("/posts").then(res => {this.setState({loadedContent: res.text()}); console.log(this.state.loadedContent)})
+    fetch("/posts").then(res => res.text()).then(result => {console.log(result)})
   }
   
   render() {
     return (
       <div className="App">
-        <div className="header bg-amber-200 fixed opacity-80 block">
+        <div className="content block">
+          { this.state.loadedContent ? this.state.loadedContent.map((item) => <div>{ item === 1 ? "" : <hr className='border-t-amber-300 border-t-4 border-dashed'></hr> }<Post key={item} image="" title="Hello" date={item} content={loremIpsum["loremIpsum"]}/></div>) : <h1>Loading</h1>}
+        </div>
+        <div className="header bg-amber-200 fixed opacity-80 block bottom-0">
           <ul>
             <li className='header-content p-4'>Blog</li>
             <li className='header-content p-4 absolute right-0'><a href="index.html">Home</a></li>
           </ul>
-        </div>
-        <div className="content block">
-          { this.state.loadedContent ? this.state.loadedContent.map((item) => <div>{ item === 1 ? "" : <hr className='border-t-amber-300 border-t-4 border-dashed'></hr> }<Post key={item} image="" title="Hello" date={item} content={loremIpsum["loremIpsum"]}/></div>) : <h1>Loading</h1>}
         </div>
       </div>
     )
